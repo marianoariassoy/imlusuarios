67b6c8f04859758eaca2b06d6edb5077085c9ac3
marianoariassoy <hola@marianoarias.soy>
2025-03-12 18:37:13 -0300

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,11 @@
-const Header = ({ title, subtitle = '', description = '', emoji = '' }) => {
+const Header = ({ title, description = '', emoji = '' }) => {
   return (
-    <header className='text-center flex flex-col'>
+    <header className='text-center flex flex-col px-6'>
       {emoji && <div className='text-2xl'>{emoji}</div>}
 
-      <div className='flex gap-x-2 justify-center text-lg px-6'>
-        <h1 className='font-bold text-primary whitespace-nowrap'>{title}</h1>
-        {subtitle && <span className='font-medium text-primary whitespace-nowrap'>{subtitle}</span>}
-      </div>
+      <h1 className='font-bold text-primary whitespace-nowrap text-lg'>{title}</h1>
 
-      <div className='font-medium text-sm'>{description}</div>
+      <div className='font-medium'>{description}</div>
     </header>
   )
 }
