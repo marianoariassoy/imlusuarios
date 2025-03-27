1f1be93de334db51c3264faf520701ee2b1de644
marianoariassoy <hola@marianoarias.soy>
2025-03-27 15:52:21 -0300

    change logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,8 @@ const Header = ({ title, description = '', emoji = '' }) => {
   return (
     <header className='text-center flex flex-col px-6'>
       {emoji && <div className='text-2xl'>{emoji}</div>}
-
       <h1 className='font-bold text-primary whitespace-nowrap text-lg'>{title}</h1>
-
-      <div className='font-medium'>{description}</div>
+      <div className='font-medium text-sm'>{description}</div>
     </header>
   )
 }
